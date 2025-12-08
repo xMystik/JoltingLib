b58e0f7585e4d3d2a8e18d1bd9190ac0c7408dfc
Skript <giannis.tiniakos99@gmail.com>
2025-12-08 16:10:47 +0200

    Update JoltingLib.java

--- src/main/java/me/skript/joltinglib/JoltingLib.java
+++ src/main/java/me/skript/joltinglib/JoltingLib.java
@@ -1,15 +1,9 @@
 package me.skript.joltinglib;
 
-import me.skript.joltinglib.bossbars.JBossBar;
 import me.skript.joltinglib.configurations.JYML;
 import me.skript.joltinglib.configurations.JFilesManager;
 import me.skript.joltinglib.glow.JGlow;
-import me.skript.joltinglib.holograms.JHologramBuilder;
-import org.bukkit.boss.BarColor;
-import org.bukkit.boss.BarStyle;
 import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.entity.TextDisplay;
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -26,34 +20,7 @@ public final class JoltingLib extends JavaPlugin implements Listener {
     public void onEnable() {
         instance = this;
         this.glowManager = new JGlow(this);
-
         this.configurationFile = filesManager.createYML("configuration");
-
-        // Register the block break hologram listener
-        getServer().getPluginManager().registerEvents(new Listener() {
-
-            @EventHandler
-            public void onBlockBreak(org.bukkit.event.block.BlockBreakEvent event) {
-                JBossBar.createBossBar(event.getPlayer().getUniqueId().toString(), "My Bossbar", BarColor.GREEN, BarStyle.SOLID);
-                JBossBar.showBossBar(event.getPlayer().getUniqueId().toString(), event.getPlayer());
-
-                var block = event.getBlock();
-                var location = block.getLocation().add(0.5, 1.5, 0.5);
-
-                String name = block.getType().name().toLowerCase().replace("_", " ");
-
-                new JHologramBuilder(location)
-                        .addLine("<#02d63b><bold>" + name + " <dark_red><italic>Test #2")
-                        .addLine("<dark_aqua><bold>Another cute line!")
-                        .setDuration(100)
-                        .setAlignment(TextDisplay.TextAlignment.CENTER)
-                        .setFloatHeight(5)
-                        .build();
-
-                JBossBar.animateProgress(event.getPlayer().getUniqueId().toString(), 0, 1, 10, 1, JoltingLib.getInstance());
-            }
-
-        }, this);
 
         this.getLogger().log(Level.INFO, "[JoltingLib] Library has been enabled!");
     }
